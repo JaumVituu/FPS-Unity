718ed64854be6562b64a59eaa167e47b887e7e98
JaumVituu <vitor40joao@gmail.com>
2021-07-02 17:43:37 -0300

    Backup Inicial

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    public float MouseX = 0f, MouseY = 0f;
+    public Vector3 offset;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+        MouseX += Input.GetAxis("Mouse X")* 2;
+        while (MouseY > -90 && MouseY < 90) {
+            MouseY -= Input.GetAxis("Mouse Y") * 2;
+            break;
+        }
+        transform.eulerAngles = new Vector3(MouseY, MouseX, 0);
+
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 tra;
+    public Vector3 offset;
+    public float RotX = 0f, RotY = 0f;
+    public GameObject Camera;
+    public Vector3 Rot;
+    
+    void Start()
+    {
+        tra.x = 0f;
+        tra.y = 0f;
+        tra.z = 0f;
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            tra.z = 0.1f;
+            Movimento(tra);
+            tra.z = 0f;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            tra.z = -0.1f;
+            Movimento(tra);
+            tra.z = 0f;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            tra.x = -0.1f;
+            Movimento(tra);
+            tra.x = 0f;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            tra.x = 0.1f;
+            Movimento(tra);
+            tra.x = 0f;
+        }
+
+        Rot.y = Input.GetAxis("Mouse X") * 2;
+        transform.Rotate(Rot);
+        
+        
+
+    }
+    
+    void Movimento(Vector3 tra)
+    {
+        transform.Translate(tra);
+    }
+}
